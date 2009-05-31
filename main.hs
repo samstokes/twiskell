8fb6a892b95463cd04c988da3074710f6a115552
Sam Stokes <sam@35cr.sc.ss496.co.uk>
2009-05-31 04:19:22 +0100

    Add funky command-line arg parsing to specify number of result on CLI

--- main.hs
+++ main.hs
@@ -1,4 +1,5 @@
 module Main where
+import System.Console.GetOpt
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
 import System.IO (hPutStrLn, stderr)
@@ -9,13 +10,35 @@ formatTweet :: Tweet -> String
 formatTweet tweet = '@' : fromUser tweet ++ ": " ++ tweetText tweet
 
 ----- Handy command-line search client -----
+usage = "Usage: twitsearch <OPTIONS> term [term ...]"
+
 main = do
   args <- getArgs
-  if (null args)
-    then err "Usage: twitsearch term [term ...]"
-    else do
-      results <- search $ basicOptions $ unwords args
-      sequence $ map putStrLn $ map formatTweet $ tweets results
+  options <- parseArgs args
+  results <- search options
+  sequence $ map putStrLn $ map formatTweet $ tweets results
+
+options :: [OptDescr Integer]
+options = [Option ['n'] ["max"] (ReqArg read "NUM") "max results (default 15)"]
+
+parseArgs :: [String] -> IO SearchOptions
+parseArgs argv =
+  case getOpt' Permute options argv of
+    ([count], term:terms, [], []) -> return SearchOptions {
+      resultsPerPage = count,
+      searchQuery = unwords (term:terms)
+      }
+    ([], term:terms, [], []) -> return $ basicOptions $ unwords (term:terms)
+    (_, _, unrecog:unrecogs, _) -> do
+      sequence $ map unrecognised (unrecog:unrecogs)
+      err $ usageInfo usage options
+    (_, _, _, errr:errrs) -> do
+      sequence $ map putStrLn (errr:errrs)
+      err $ usageInfo usage options
+    _ -> err $ usageInfo usage options
+
+unrecognised :: String -> IO ()
+unrecognised opt = putStrLn ("Unrecognised option " ++ opt)
 
 err :: String -> IO a
 err msg = do 
